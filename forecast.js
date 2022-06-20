da4b0237483e25fb74b3aac59ec53db73d878853
chloebc9 <chloebc101@gmail.com>
2022-06-20 13:43:40 +0100

    Update forecast.js

--- forecast.js
+++ forecast.js
@@ -54,3 +54,5 @@ function getForecast(coordinates) {
   let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${key}&units=metric`;
   axios.get(url).then(displayForecast);
 }
+
+searchCity("London");
